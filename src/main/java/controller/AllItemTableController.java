2183e0d753b4f742bb4020d4591abde270bb03b7
isura <isura3147@gmail.com>
2025-08-14 18:52:38 +0530

    added all item table summary

--- src/main/java/controller/AllItemTableController.java
+++ src/main/java/controller/AllItemTableController.java
@@ -1,4 +1,92 @@
 package controller;
 
-public class AllItemTableController {
+import com.jfoenix.controls.JFXButton;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.dto.Item;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+
+public class AllItemTableController implements Initializable {
+
+    @FXML
+    private JFXButton btnBack;
+
+    @FXML
+    private TableColumn<?, ?> colDescription;
+
+    @FXML
+    private TableColumn<?, ?> colItemCode;
+
+    @FXML
+    private TableColumn<?, ?> colPackSize;
+
+    @FXML
+    private TableColumn<?, ?> colUnitPrice;
+
+    @FXML
+    private TableColumn<?, ?> colQtyOnHand;
+
+    @FXML
+    public TableView<Item> tblItems;
+
+    private Stage stage = new Stage();
+    private Stage currentStage;
+
+    @FXML
+    void btnBackOnAction(ActionEvent event) {
+        currentStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/update_item_form.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+        currentStage.close();
+    }
+
+    ObservableList<Item> itemInfos = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade", "root", "isura1234");
+            String SQL = "SELECT * FROM items;";
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Item itemInfo = new Item(
+                        resultSet.getString("item_code"),
+                        resultSet.getString("description"),
+                        resultSet.getString("pack_size"),
+                        resultSet.getDouble("unit_price"),
+                        resultSet.getInt("quantity_on_hand")
+                );
+                itemInfos.add(itemInfo);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        colItemCode.setCellValueFactory(new PropertyValueFactory<>("itemCode"));
+        colDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+        colPackSize.setCellValueFactory(new PropertyValueFactory<>("packSize"));
+        colUnitPrice.setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
+        colQtyOnHand.setCellValueFactory(new PropertyValueFactory<>("qtyOnHand"));
+        tblItems.setItems(itemInfos);
+    }
 }
